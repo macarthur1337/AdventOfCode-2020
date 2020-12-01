a5018d7ba68501f690fc174a0ab3f26320397b72
Neil E. Absil <neil@keystone-strategic.com>
2020-12-01 20:35:40 +0100

    *initial commit + first days solutions.

---
+++ src/main/kotlin/macarthur1337/day1/Part1.kt
@@ -0,0 +1,40 @@
+package macarthur1337.day1
+
+import org.springframework.util.ResourceUtils
+import java.io.InputStream
+
+/**
+ * @author N. E. Absil
+ * @since 30-11-2020
+ */
+fun main() {
+    val inputStream: InputStream = ResourceUtils.getFile("classpath:input1").inputStream()
+    val lineList = mutableListOf<Int>()
+
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it.toInt()) } }
+
+    val result = find2020(0, lineList)
+
+    println("Found: $result")
+}
+
+/**
+ * recursively find solution
+ */
+fun find2020(index: Int, list: MutableList<Int>) : Int {
+    val candidate = list[index]
+    println("trying $candidate")
+    list.forEach { if(it + candidate == 2020){
+        println("Found: $candidate + $it")
+        return candidate * it
+        }
+    }
+    val newCandidate = index + 1
+    return if(newCandidate <= list.size -1){
+        find2020(newCandidate, list)
+    }
+    else{
+        println("No candidates found")
+        0
+    }
+}
---
+++ src/main/kotlin/macarthur1337/day1/Part2.kt
@@ -0,0 +1,35 @@
+package macarthur1337.day1
+
+import org.springframework.util.ResourceUtils
+import java.io.InputStream
+
+/**
+ * @author N. E. Absil
+ * @since 30-11-2020
+ */
+fun main() {
+    val inputStream: InputStream = ResourceUtils.getFile("classpath:input1").inputStream()
+    val lineList = mutableListOf<Int>()
+
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it.toInt()) } }
+
+    //brute force / random walk
+    var found = true
+    while(found){
+        val first = lineList.shuffled()
+        val second = lineList.shuffled()
+        val third = lineList.shuffled()
+
+        var index = 0
+        while(index < lineList.size-1){
+            if(first[index] + second[index] + third[index] == 2020){
+                println("Found ${first[index]} + ${second[index]} + ${third[index]}")
+                val result = first[index] * second[index] * third[index]
+                println("Found: $result")
+                found = false
+                break
+            }
+            index++
+        }
+    }
+}
